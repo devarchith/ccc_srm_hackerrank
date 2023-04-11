d742c00a6c2e1f5397adb555091d5a3d88816670
devarchith <98102667+devarchith@users.noreply.github.com>
2023-04-11 16:15:34 +0530

    Create AMAZON_13.cpp

---
+++ AMAZON_13.cpp
@@ -0,0 +1,35 @@
+//Write a function to find the longest common prefix string amongst an array of strings.
+
+//If there is no common prefix, return an empty string "".
+
+//Input Format
+
+//The first line contains the size of array
+//The Second line contains an array of strings
+//Constraints
+
+//1 <= strs.length <= 200
+//0 <= strs[i].length <= 200
+//strs[i] consists of only lowercase English letters.
+//Output Format
+
+//Print a string which is the answer to the question
+
+//Sample Input 0
+
+//3
+//flow
+//flow
+//flower
+//Sample Output 0
+
+//flow
+//Sample Input 1
+
+//3
+//bwjepxu
+//beehgn
+//bevrvwov
+//Sample Output 1
+
+//b
